b52fa6fad559ff526720c9ff9ace2fe7b2dc9c59
Rob Bricheno <rob@ambrosia.org.uk>
2019-01-10 12:51:01 +0000

    Add missing config variables

--- internal/loralogger/config.go
+++ internal/loralogger/config.go
@@ -2,5 +2,7 @@ package loralogger
 
 // Config holds the loralogger config.
 type Config struct {
-	Bind string `mapstructure:"bind"`
+	Bind   string `mapstructure:"bind"`
+	Region string `mapstructure:"region"`
+	Table  string `mapstructure:"table"`
 }
